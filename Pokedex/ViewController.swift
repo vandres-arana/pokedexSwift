ee219c885bfdf2fdff73337704601e0f53ff8dde
Cristian.Almendro <cristian.almendro@jala-foundation.org>
2021-04-22 15:20:51 -0400

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Cristian Misael Almendro Lazarte on 22/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
