73a83f4f9d68311cd3a99f889970c5be89fb9c23
Andres.Arana <andres.arana@jala-foundation.org>
2021-04-26 11:49:05 -0400

    GraphQL Apollo Library for PokeApi added to the project

---
+++ Pokedex/NetworkingServices/PokeApiService.swift
@@ -0,0 +1,15 @@
+//
+//  ListPokeApiService.swift
+//  Pokedex
+//
+//  Created by Victor Arana on 4/25/21.
+//
+
+import Foundation
+import Apollo
+
+class PokeApiService {
+  static let shared = PokeApiService()
+    
+  private(set) lazy var apollo = ApolloClient(url: URL(string: "https://beta.pokeapi.co/graphql/v1beta")!)
+}
