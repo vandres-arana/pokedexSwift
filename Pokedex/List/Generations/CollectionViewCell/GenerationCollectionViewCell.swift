b3a3ce8fcc9175b77628a20798d75bba7954c2a7
Cristian.Almendro <cristian.almendro@jala-foundation.org>
2021-04-26 21:19:03 -0400

    Deleting logic in the generation cell view

--- Pokedex/List/Generations/CollectionViewCell/GenerationCollectionViewCell.swift
+++ Pokedex/List/Generations/CollectionViewCell/GenerationCollectionViewCell.swift
@@ -14,54 +14,7 @@ class GenerationCollectionViewCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-        
-        let view = UILabel()
-
-        view.frame = CGRect(x: 0, y: 0, width: 110, height: 110)
-
-        view.backgroundColor = .white
-
-
-        let layer0 = CAGradientLayer()
-
-        layer0.colors = [
-
-          UIColor(red: 0.925, green: 0.925, blue: 0.925, alpha: 1).cgColor,
-
-          UIColor(red: 0.961, green: 0.961, blue: 0.961, alpha: 1).cgColor
-
-        ]
-
-        layer0.locations = [0, 1]
-
-        layer0.startPoint = CGPoint(x: 0.25, y: 0.5)
-
-        layer0.endPoint = CGPoint(x: 0.75, y: 0.5)
-
-        layer0.transform = CATransform3DMakeAffineTransform(CGAffineTransform(a: 1, b: 1, c: -1, d: 1, tx: 0.5, ty: -0.5))
-
-        layer0.bounds = view.bounds.insetBy(dx: -0.5*view.bounds.size.width, dy: -0.5*view.bounds.size.height)
-
-        layer0.position = view.center
-
-        view.layer.addSublayer(layer0)
-
-
-
-        let parent = View!
-
-        parent.addSubview(view)
-
-        view.translatesAutoresizingMaskIntoConstraints = false
-
-        view.widthAnchor.constraint(equalToConstant: 110).isActive = true
-
-        view.heightAnchor.constraint(equalToConstant: 110).isActive = true
-
-        view.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 60).isActive = true
-
-        view.topAnchor.constraint(equalTo: parent.topAnchor, constant: 70).isActive = true
+        // Initialization code 
     }
 
 }
