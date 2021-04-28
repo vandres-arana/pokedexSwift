ef532bfd93c5bcb480804c67183898d0570e640f
Andres.Arana <andres.arana@jala-foundation.org>
2021-04-28 11:03:12 -0400

    Filter by type view created

---
+++ Pokedex/List/FilterMenu/FilterMenu.swift
@@ -0,0 +1,16 @@
+//
+//  FilterMenu.swift
+//  Pokedex
+//
+//  Created by Victor Arana on 4/27/21.
+//
+
+import UIKit
+
+class FilterMenu: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
