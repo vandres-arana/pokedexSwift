6cbc16442e32ccb3540633ae0d3525016dfab9eb
Cristian.Almendro <cristian.almendro@jala-foundation.org>
2021-04-29 09:39:47 -0400

    fixing bugs

--- Pokedex/List/Generations/CollectionView/CollectionViewCell/GenerationCollectionViewCell.swift
+++ Pokedex/List/Generations/CollectionView/CollectionViewCell/GenerationCollectionViewCell.swift
@@ -2,20 +2,21 @@
 //  GenerationCollectionViewCell.swift
 //  Pokedex
 //
-//  Created by Cristian Misael Almendro Lazarte on 26/4/21.
+//  Created by Cristian Misael Almendro Lazarte on 29/4/21.
 //
 
 import UIKit
 
 class GenerationCollectionViewCell: UICollectionViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib();
     }
     
-    
-    func updateUI(name: String) {
-        generationLabel.text = name;
+    func setlabel(name: String) {
+        label.text = name;
     }
+    
 }
--- Pokedex/List/Generations/CollectionView/GenerationsView.swift
+++ Pokedex/List/Generations/CollectionView/GenerationsView.swift
@@ -14,28 +14,31 @@ class GenerationsView: UIViewController {
     // MARK: Properties
     var presenter: GenerationsPresenterProtocol?
     var generations: [GetGenerationsQuery.Data.Generation] = [];
-    var generationsT: [String] = ["1","2"];
-    
     @IBOutlet weak var generationCollectionView: UICollectionView!
     
     // MARK: Lifecycle
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
+        super.viewDidLoad() 
         presenter?.viewDidLoad()
-        
-        PokeApiService.shared.apollo.fetch(query: GetGenerationsQuery()) { result in
-            switch result {
-            case .success(let getGenerationsQuery) :
-                self.generations = getGenerationsQuery.data!.generations;
-                print(self.generations)
-                break;
-            case .failure(let error) :
-                print(error);
-                break;
-            }
-        }
+//
+//        PokeApiService.shared.apollo.fetch(query: GetGenerationsQuery()) { result in
+//            switch result {
+//            case .success(let getGenerationsQuery) :
+//                self.generations = getGenerationsQuery.data!.generations;
+//
+//                DispatchQueue.main.async {
+//                    self.generationCollectionView.reloadData()
+//                }
+//
+//                print(self.generations)
+//
+//                break;
+//            case .failure(let error) :
+//                print(error);
+//                break;
+//            }
+//        }
         
     }
 }
@@ -53,15 +56,15 @@ extension GenerationsView: GenerationsViewProtocol {
 extension GenerationsView : UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return generationsT.count;
+        return generations.count;
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.IdentifierReusableCell.generationReusableCell, for: indexPath) as! GenerationCollectionViewCell;
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "generationCollectionViewCell", for: indexPath) as! GenerationCollectionViewCell ;
         
-        cell.updateUI(name: generationsT[indexPath.row]);
-        
+        //cell.setlabel(name: "test");
+        cell.setlabel(name: self.generations[indexPath.row].name);
         return cell;
     }
 }
