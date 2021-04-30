25e5cc26d219347e5226739f0fc5c332fa72c038
Christian Torrico <christorricoavila@gmail.com>
2021-04-30 07:57:32 -0400

    base files for custom cell

---
+++ Pokedex/List/Custom Cell/CustomPokemonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomPokemonTableViewCell.swift
+//  Pokedex
+//
+//  Created by user on 4/30/21.
+//
+
+import UIKit
+
+class CustomPokemonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
