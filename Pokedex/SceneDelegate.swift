3dcba8ef6034a63ada2789cf9b437e3497fb6561
Cristian.Almendro <cristian.almendro@jala-foundation.org>
2021-05-05 08:39:59 -0400

    Restoring the List view for principal view

--- Pokedex/SceneDelegate.swift
+++ Pokedex/SceneDelegate.swift
@@ -17,9 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
-        let generationWireFrame = GenerationsWireFrame.createGenerationsModule()
+        let listWireFrame = ListWireFrame.createListModule()
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = generationWireFrame
+        window?.rootViewController = listWireFrame
         window?.makeKeyAndVisible()
     }
 
